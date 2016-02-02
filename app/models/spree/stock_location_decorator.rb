61584c8e32f65cb189aac57c122719fd257acfcc
Denis <denguk@gmail.com>
2016-02-02 19:42:28 +0000

    fix: stock model

--- app/models/spree/stock_location_decorator.rb
+++ app/models/spree/stock_location_decorator.rb
@@ -2,7 +2,7 @@ Spree::StockLocation.class_eval do
 
   # Wrapper for creating a new stock item respecting the backorderable config and supplier
   durably_decorate :propagate_variant, mode: 'soft', sha: 'f488d0a2cd154b1380cef9ee8c4db004fc825eb4' do |variant|
-    if self.supplier_id.blank? || variant.supplier_id == self.supplier_id
+    if self.supplier_id.blank? || variant.product.supplier_id == self.supplier_id
       self.stock_items.create!(variant: variant, backorderable: self.backorderable_default)
     end
   end
